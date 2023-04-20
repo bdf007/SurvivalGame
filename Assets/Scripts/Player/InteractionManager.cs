b8e99096a6e41fad05783beec710cfc0a01851a1
bdf007 <bdf007@gmail.com>
2023-04-20 19:12:33 +0200

    adding the UI for the interactable object

--- Assets/Scripts/Player/InteractionManager.cs
+++ Assets/Scripts/Player/InteractionManager.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 
 public class InteractionManager : MonoBehaviour
 {
@@ -26,6 +28,49 @@ public class InteractionManager : MonoBehaviour
         if (Time.time - lastCheckTime > checkRate)
         {
             lastCheckTime = Time.time;
+
+            // create a ray from the center of our screen pointing in the direction we're looking
+            Ray ray = cam.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
+            RaycastHit hit;
+
+            // did we hit something?
+            if (Physics.Raycast(ray, out hit, maxCheckDistance, layerMask))
+            {
+                // is this not our current interactable?
+                // if so, set it as our current interactable
+                if (hit.collider.gameObject != curInteractGameObject)
+                {
+                    // set our current interactable
+                    curInteractGameObject = hit.collider.gameObject;
+                    curInteractable = hit.collider.GetComponent<IInteractable>();
+                    // set our prompt text
+                    SetPromptText();
+                }
+            }
+            else
+            {
+                curInteractGameObject = null;
+                curInteractable = null;
+                promptText.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    // called when we hover over a new interactable
+    void SetPromptText()
+    {
+        promptText.gameObject.SetActive(true);
+        promptText.text = string.Format("<b>[E]<b> {0}", curInteractable.GetInteractPrompt());
+    }
+
+    public void OnInterractInput(InputAction.CallbackContext context)
+    {
+        if(context.phase == InputActionPhase.Started && curInteractable != null)
+        {
+            curInteractable.OnInteract();
+            curInteractGameObject = null;
+            curInteractable = null;
+            promptText.gameObject.SetActive(false);
         }
     }
 }
