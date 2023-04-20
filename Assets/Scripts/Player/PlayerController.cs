4f59e27e03682b5ebb6fbb1ea7d7997e8cdcc3c9
bdf007 <bdf007@gmail.com>
2023-04-20 11:00:02 +0200

    adding movement to our player

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,6 +5,11 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Movement")]
+    public float moveSpeed;
+    private Vector2 curMovementInput;
+
+    [Header("Look")]
     public Transform cameraContainer;
     public float minXLook;
     public float maxXLook;
@@ -14,11 +19,36 @@ public class PlayerController : MonoBehaviour
 
     private Vector2 mousDelta;
 
+    // components
+    private Rigidbody playerRig;
+
+    private void Awake()
+    {
+        // get the components
+        playerRig = GetComponent<Rigidbody>();
+    }
+
     private void Start()
     {
         // lock the cursor to the center of the screen
         Cursor.lockState = CursorLockMode.Locked;
 
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move ()
+    {
+        // calculate the move direction relative to where we're facing.
+        Vector3 moveDir = transform.forward * curMovementInput.y + transform.right * curMovementInput.x;
+        moveDir *= moveSpeed;
+        moveDir.y = playerRig.velocity.y;
+
+        // aasign our rigidbody velocity to the move direction
+        playerRig.velocity = moveDir;
     }
 
     private void LateUpdate()
@@ -49,5 +79,22 @@ public class PlayerController : MonoBehaviour
     public void OnLookInput(InputAction.CallbackContext context)
     {
         mousDelta = context.ReadValue<Vector2>();
+    }
+
+    // called when we press WASD - managed by the Input System
+    public void OnMoveInput(InputAction.CallbackContext context)
+    {
+        // are we holding down a movement button?
+        if (context.performed)
+        {
+            // get the movement input
+            curMovementInput = context.ReadValue<Vector2>();
+        }
+        // are we releasing a movement button?
+        else if (context.canceled)
+        {
+            // reset the movement input
+            curMovementInput = Vector2.zero;
+        }
     }
 }
