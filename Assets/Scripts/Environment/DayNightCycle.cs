017b7b92d84527fdb35a10bb16a33570ea2ea870
bdf007 <bdf007@gmail.com>
2023-04-20 17:20:41 +0200

    complet the scripts for DayNightCycle

--- Assets/Scripts/Environment/DayNightCycle.cs
+++ Assets/Scripts/Environment/DayNightCycle.cs
@@ -14,4 +14,63 @@ public class DayNightCycle : MonoBehaviour
     public Light sun;
     public Gradient sunColor;
     public AnimationCurve sunIntensity;
+
+    [Header("Moon")]
+    public Light moon;
+    public Gradient moonColor;
+    public AnimationCurve moonIntensity;
+
+    [Header("Other Lighting")]
+    public AnimationCurve lightingIntensityMultiplier;
+    public AnimationCurve reflectionIntensityMultiplier;
+
+    private void Start()
+    {
+        timeRate = 1.0f / fullDayLength;
+        time = startTime;
+    }
+
+    private void Update()
+    {
+        // incremnent time
+        time += timeRate * Time.deltaTime;
+
+        // loop time
+        if (time > 1.0f)
+        {
+            time = 0.0f;
+        }
+
+        // light rotation
+        sun.transform.eulerAngles = (time - 0.25f) * noon * 4.0f;
+        moon.transform.eulerAngles = (time - 0.75f) * noon * 4.0f;
+
+        // light intensity
+        sun.intensity = sunIntensity.Evaluate(time);
+        moon.intensity = moonIntensity.Evaluate(time);
+
+        // change colors
+        sun.color = sunColor.Evaluate(time);
+        moon.color = moonColor.Evaluate(time);
+
+        // enable/disable sun
+        if (sun.intensity == 0 && sun.gameObject.activeInHierarchy)
+        {
+            sun.gameObject.SetActive(false);
+        }
+        else if (sun.intensity > 0 && !sun.gameObject.activeInHierarchy)
+        {
+            sun.gameObject.SetActive(true);
+        }
+
+        // enable/disable moon
+        if (moon.intensity == 0 && moon.gameObject.activeInHierarchy)
+        {
+            moon.gameObject.SetActive(false);
+        }
+        else if (moon.intensity > 0 && !moon.gameObject.activeInHierarchy)
+        {
+            moon.gameObject.SetActive(true);
+        }
+    }
 }
