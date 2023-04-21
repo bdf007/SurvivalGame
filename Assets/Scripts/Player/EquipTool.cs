c31400afa545c195f9a572d14182d6f0de6f694e
bdf007 <bdf007@gmail.com>
2023-04-21 11:21:27 +0200

    attack function

--- Assets/Scripts/Player/EquipTool.cs
+++ Assets/Scripts/Player/EquipTool.cs
@@ -5,4 +5,45 @@ using UnityEngine;
 public class EquipTool : Equip
 {
 
+    public float attackRate;
+    private bool isAttacking;
+    public float attackDistance;
+
+
+    [Header("Ressource Gathering")]
+    public bool doesGatherRessources;
+
+    [Header("Combat")]
+    public bool doesDamage;
+    public int damage;
+
+    // components
+    private Animator animator;
+    private Camera cam;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        cam = Camera.main;
+    }
+
+    public override void OnAttackInput()
+    {
+        if (!isAttacking)
+        {
+            isAttacking = true;
+            animator.SetTrigger("Attack");
+            Invoke("OnAttack", attackRate);
+        }
+    }
+
+    void OnAttack()
+    {
+        isAttacking = true;
+    }
+    
+    public void Onhit()
+    {
+
+    }
 }
