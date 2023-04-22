2d838e3b1ea59e2fbc79a00286543114df0114c1
bdf007 <bdf007@gmail.com>
2023-04-22 12:32:55 +0200

    implementation du scrip campFire

--- Assets/Scripts/Placeables/CampFire.cs
+++ Assets/Scripts/Placeables/CampFire.cs
@@ -8,20 +8,73 @@ public class CampFire : Building, IInteractable
     public GameObject particle;
     public GameObject lightFire;
     private bool isOn;
+    private Vector3 lightFireStartPos;
 
     [Header("Damage")]
     public int damage;
     public float damageRate;
 
-    private List<IInteractable> thingsToDamage = new List<IInteractable>();
+    private List<IDamageable> thingsToDamage = new List<IDamageable>();
+
+    void Start()
+    {
+        lightFireStartPos = lightFire.transform.localPosition;
+        StartCoroutine(DealDamage());
+    }
+
+    IEnumerator DealDamage()
+    {
+        while (true)
+        {
+            if(isOn)
+            {
+                for (int i = 0; i < thingsToDamage.Count; i++)
+                {
+                    thingsToDamage[i].TakePhysicalDamage(damage);
+                }
+            }
+            
+            yield return new WaitForSeconds(damageRate);
+        }
+    }
+    
     public string GetInteractPrompt()
     {
-        throw new System.NotImplementedException();
+        return isOn ? "Turn Off" : "Turn On";
     }
 
     public void OnInteract()
     {
-        throw new System.NotImplementedException();
+        isOn = !isOn;
+        lightFire.SetActive(isOn);
+        particle.SetActive(isOn);
+    }
+
+    void Update()
+    {
+        if (isOn)
+        {
+            float x = Mathf.PerlinNoise(Time.time * 3.0f, 0.0f) / 5.0f;
+            float z = Mathf.PerlinNoise(0.0f, Time.time * 3.0f) / 5.0f;
+
+            lightFire.transform.localPosition = lightFireStartPos + new Vector3(x, 0.0f, z);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.GetComponent<IDamageable>() != null)
+        {
+            thingsToDamage.Add(other.gameObject.GetComponent<IDamageable>());
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.GetComponent <IDamageable>() != null)
+        {
+            thingsToDamage.Remove(other.gameObject.GetComponent<IDamageable>());
+        }
     }
 }
 
