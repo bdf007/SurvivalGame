906692c8ad438f824211962d08243c0b4b0b1d0a
bdf007 <bdf007@gmail.com>
2023-04-22 14:08:24 +0200

    create the npc script and implement the variable

---
+++ Assets/Scripts/NPC/NPC.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public enum AIType
+{
+    Passive,
+    Scared,
+    Aggressive
+}
+
+public enum AIState
+{
+    Idle,
+    Wandering,
+    Attacking,
+    Fleeing
+}
+public class NPC : MonoBehaviour
+{
+
+    [Header("stats")]
+    public int health;
+    public float walkSpeed;
+    public float runSpeed;
+    public ItemData[] dropOnDeath;
+
+    [Header("AI")]
+    public AIType aiType;
+    private AIState aiState;
+    public float detectDistance;
+    public float safeDistance;
+
+    [Header("Wandering")]
+    public float minWanderDistance;
+    public float maxWanderDistance;
+    public float minWanderWaitTime;
+    public float maxWanderWaitTime;
+
+    [Header("Combat")]
+    public int damage;
+    public float attackRate;
+    public float lastAttackTime;
+    public float attackDistance;
+
+    private float playerDistance;
+
+    // components
+    private NavMeshAgent agent;
+    private Animator anim;
+    private SkinnedMeshRenderer[] meshRenderers;
+
+}
